9c4238b5e4d96249ca0c9bf7103c65018c70774a
thunder775 <rahulmasih61@gmail.com>
2021-01-07 14:54:27 +0530

    done

--- client.js
+++ client.js
@@ -1,6 +1,25 @@
 const io = require("socket.io-client");
 const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin,  output: process.stdout });
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
 // write your code here
+const endpoint = 'http://localhost:3000'
+
+let username
+let socket
+rl.question("What is your name?", function (name) {
+    username = name
+    console.log("Successfully connected to a server");
+    socket = io.io(endpoint);
+    socket.on('simple chat message', function (data) {
+        console.log((data));
+    });
+});
+
+
+rl.addListener("line", (line) => {
+    socket.emit("simple chat message", {
+        message: `${username} says ${line}`,
+    });
+});
 
